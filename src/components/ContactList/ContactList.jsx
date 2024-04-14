76e3157ba0fe5202465a7c68b3f51470defe98ee
Lilia Levchenko <liliya.benatti@gmail.com>
2024-04-14 16:28:35 +0200

    upd

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,23 @@
 import css from "./ContactList.module.css";
 import { Contact } from "../Contact/Contact";
+import { deleteContact, selectContacts } from "../../redux/contactsSlice";
 
-export const ContactList = ({ contacts, onDelete }) => {
+import { useDispatch, useSelector } from "react-redux";
+
+export const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
+
+  const handleDelete = (contactId) => {
+    dispatch(deleteContact(contactId));
+  };
+
   return (
     <ul className={css.list}>
       {contacts.map((contact) => {
         return (
           <li key={contact.id} className={css.item}>
-            <Contact data={contact} onDelete={onDelete} />
+            <Contact data={contact} onDelete={() => handleDelete(contact.id)} />
           </li>
         );
       })}
